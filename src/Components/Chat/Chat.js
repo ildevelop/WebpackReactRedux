e43fb5a7dbcce40bc9317632b97a2f82aefa7dc9
Ilya Radu <raduilya@gmail.com>
2017-05-31 23:23:18 +0300

    Add message box

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -6,27 +6,60 @@ import './Chat.scss'
 import {addNewUser} from "../../Store/ActionCreator";
 
 export class Chat extends React.Component {
-    constructor(){
+    constructor() {
         super();
-        this.state ={
+        this.state = {
             userName: 'ILYA'
         }
     }
-    addNewName(event){
-        const title= event.target.value;
-        this.setState({userName:title})
+
+    addNewName(event) {
+        const title = event.target.value;
+        this.setState({userName: title})
     }
 
     render() {
-        const {dispatch ,users} = this.props;
+        const {dispatch, users} = this.props;
         return (
-            <div>
-                <input type="text" onChange={this.addNewName.bind(this)}/>
-                <button onClick={(userName)=> dispatch(addNewUser(this.state.userName))}>ADD NEW USER</button>
-                {users.map((i,key)=>
-                    <p key={key}>Hello {i}</p>
+            <div className="MainMessage">
+                <div className="allMessage">
+                    {/*{users.map((i,key)=>*/}
+                    {/*<p  key={key}>Hello {i}</p>*/}
+                    {/*)}*/}
+                    <div className="clearfix">
+                        <blockquote className="me">Hi</blockquote>
+                    </div>
+                    <div className="clearfix">
+                        <blockquote className="you">Hello</blockquote>
+                    </div>
+                    <div className="clearfix">
+                        <blockquote className="me">wass up !</blockquote>
+                    </div>
+                    <div className="clearfix">
+                        <blockquote className="you">Film was up u!</blockquote>
+                    </div>
+                    <div className="clearfix">
+                        <blockquote className="me">Just outing</blockquote>
+                    </div>
+                    <div className="clearfix">
+                        <blockquote className="you">Hello how do u do ?</blockquote>
+                    </div>
+                    <div className="clearfix">
+                        <blockquote className="me">Yup great !</blockquote>
+                    </div>
+                </div>
+                <div className="allTeam">
+                    <div className="addUser">
+                        <input type="text" onChange={this.addNewName.bind(this)}/>
+                        <button onClick={(userName) => dispatch(addNewUser(this.state.userName))}>ADD NEW USER</button>
+                    </div>
+                    <div className="allUsers">
+                        {users.map((i, key) =>
+                            <p key={key}> {i}</p>
+                        )}
+                    </div>
+                </div>
 
-                )}
 
             </div>
         )
