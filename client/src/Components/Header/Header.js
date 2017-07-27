19d17d8440c62fae4c535a646a018e364042da1b
Ilya Radu <raduilya@gmail.com>
2017-07-27 18:19:17 +0300

    async setState

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -3,21 +3,24 @@
  */
 import React from 'react'
 import './Header.scss'
-export class Header extends React.Component{
-    constructor(){
-        super();
-        this.state = {
-            counter: 0
-        }
+
+export class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0
     }
-    render(){
-        return (
-            <div className="header1">
-                <h1 onClick={()=>this.setState({counter: this.state.counter+1})}>Hello {this.state.counter}</h1>
-            </div>
-            )
+  }
+
+  render() {
+    return (
+      <div className="header1">
+        <h1 onClick={() => {this.setState((prevState) => ({counter: prevState.counter + 1}))}}> Hello {this.state.counter}</h1>
+      </div>
+    )
 
 
-    }
+  }
 }
+
 export default Header
